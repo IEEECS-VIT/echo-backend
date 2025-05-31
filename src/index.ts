172fc469565cd639aca109cc1fd90be3dac6b3ff
arnav <arnav.mathur.1024@gmail.com>
2025-05-31 19:14:38 +0530

    made the basic route for messages , needs more work

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express, { Request, Response } from 'express'
+const messages = require('./routes/message');
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.use(express.json());
+
 app.get('/', (_req: Request, res: Response) => {
   res.send('Hello from echo-backend!')
 })
+
+app.use('/message', messages);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
---
+++ src/routes/message.ts
@@ -0,0 +1,87 @@
+/* AUTH MIDDLEWARE HAS NOT BEEN ADDED YET , please remove this line once added */
+
+
+const express = require('express');
+import type { Request, Response } from "express";
+const router = express.Router();
+
+const pg = require('pg');
+
+const {v4} = require('uuid')
+
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage }); 
+
+const {createClient} = require('@supabase/supabase-js');
+
+const supabase = createClient(
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY! // BE CAREFUL: this key should only be in backend
+);
+
+
+/*change the rest route below to the socket architecture as needed */
+
+/*IMPORTANT : implement the auth middleware*/
+
+router.post('/message', upload.single('file'), async (req:Request, res:Response)=>{
+    
+    const id = v4();
+    const {content, channelId, senderId, replyToId } = req.body;
+    if(!channelId){
+        return res.status(400).json({'error':'No channelId received.'});
+    } 
+    if(!senderId){
+        return res.status(400).json({'error':'No senderId received.'});
+    }
+
+    let mediaUrl:string | null = null;
+
+    try{
+        if (req.file) {
+            const fileExt = req.file.originalname.split('.').pop();//gets the extension of the file
+            const fileName = `${id}.${fileExt}`;//filename to store as , should not conflict.
+
+            const { data, error: uploadError } = await supabase.storage
+                .from(process.env.SUPABASE_BUCKET!)
+                .upload(fileName, req.file.buffer, {
+                contentType: req.file.mimetype,
+                upsert: true,
+            });
+
+            if (uploadError) {
+                console.error(uploadError);
+                return res.status(500).json({'error': 'Server error'});
+            }
+
+            // Get public URL
+            const { data: publicUrlData } = supabase.storage.from(process.env.SUPABASE_BUCKET!).getPublicUrl(fileName);
+            mediaUrl = publicUrlData.publicUrl;
+        }
+
+        //now store all data in "Message" table
+        const { error: insertError } = await supabase.from('Message').insert({
+            id,
+            content,
+            mediaUrl,
+            isEdited: false,
+            channelId,
+            senderId,
+            replyToId: replyToId || null,
+        });
+
+        if (insertError) {
+            console.error(insertError);
+            return res.status(500).json({error:'Server error'});
+        }
+        return res.status(200).json({msg:'Message saved successfully'});
+    } 
+    catch(error:any){
+        console.error(error);
+        return res.status(500).json({error:'Server error'});
+    }
+
+} );
+
+module.exports = router;
