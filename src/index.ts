6a3fa41231f20cb0882b2bd061bc05cf23158450
Aryan Jain <aryanrameshjain@gmail.com>
2025-05-31 13:39:27 +0530

    setup: supabase

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
+import 'dotenv/config';
 import express, { Request, Response } from 'express'
+import './client/supabase'
+import { checkBucketConnection } from './lib/storage'
 
 const app = express()
 const PORT = process.env.PORT || 5000
+
+checkBucketConnection().catch(console.error)
 
 app.get('/', (_req: Request, res: Response) => {
   res.send('Hello from echo-backend!')
