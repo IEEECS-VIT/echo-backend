645092b1e9454541cdc23c62b90b075e91587fad
Harsh Vardhan <harshvardhan.p27@gmail.com>
2025-06-12 00:32:51 +0530

    fix: added option to validate tokens from cookies
    - now validates tokens under cookies as well.

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,15 +1,25 @@
+//checks for valid token under header or cookies 
+
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret';
 
 export const authenticate = (req: Request, res: Response, next: NextFunction) => {
+  let token: string| undefined;
+
   const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith('Bearer '))
+  if (authHeader && authHeader.startsWith('Bearer ')){
+      token = authHeader.split(' ')[1];
+  }
+  else if(req.cookies &&req.cookies.access_token){
+      token = req.cookies.access_token;
+  }
+  
+  if (!token) {
     return res.status(401).json({ message: 'No token provided' });
-
-  const token = authHeader.split(' ')[1];
-
+  }
+  
   try {
     const payload = jwt.verify(token, JWT_SECRET) as { userId: string };
     (req as any).user = payload; 
