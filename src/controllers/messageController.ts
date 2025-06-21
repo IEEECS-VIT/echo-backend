0a2fe4804d96b2a74c2a11ee04cb3f37980ea5de
arnav <arnav.mathur.1024@gmail.com>
2025-06-21 18:07:27 +0530

    made small changes to POST route , POST route working properly

--- src/controllers/messageController.ts
+++ src/controllers/messageController.ts
@@ -4,19 +4,21 @@ import {v4} from 'uuid';
 
 export const messagePostController = async (req:Request, res:Response):Promise<any>=>{
     
-    const id = v4();
-    const {content, channel_id, sender_id, reply_to } = req.body;
-    if(!channel_id){
-        return res.status(400).json({'error':'No channelId received.'});
-    } 
-    if(!sender_id){
-        return res.status(400).json({'error':'No senderId received.'});
-    }
-
-    let media_url:string | null = null;
-
     try{
-        if (req.file) {
+        const id = v4();
+        const {content, channel_id, sender_id, reply_to, is_dm } = req.body;
+        const is_dm_bool = (is_dm === 'true'); //else false
+        if(!channel_id){
+            return res.status(400).json({'error':'No channelId received.'});
+        } 
+        if(!sender_id){
+            return res.status(400).json({'error':'No senderId received.'});
+        }
+
+        let media_url:string | null = null;
+
+    
+        if (req.file){
             const fileExt = req.file.originalname.split('.').pop();//gets the extension of the file
             const fileName = `${id}.${fileExt}`;//filename to store as , should not conflict.
 
@@ -33,27 +35,47 @@ export const messagePostController = async (req:Request, res:Response):Promise<a
             }
 
             // Get public URL
-            const { data: publicUrlData } = supabase.storage.from(process.env.SUPABASE_BUCKET!).getPublicUrl(fileName);
+            const { data: publicUrlData } = supabase.storage.from('attachments').getPublicUrl(fileName);
             media_url = publicUrlData.publicUrl;
+            console.log(media_url);
         }
+        
+        if(!is_dm_bool){ //for server channels
+        //store all data in "Message" table 
+            const { error: insertError } = await supabase.from('messages').insert({
+                id,
+                content,
+                media_url,
+                is_edited: false,
+                channel_id,
+                sender_id,
+                reply_to: reply_to || null,
+            });
 
-        //store all data in "Message" table
-        const { error: insertError } = await supabase.from('messages').insert({
-            id,
-            content,
-            media_url,
-            is_edited: false,
-            channel_id,
-            sender_id,
-            reply_to: reply_to || null,
-        });
+            if (insertError) {
+                console.error(insertError);
+                return res.status(500).json({error:'Server error'});
+            }
+        }
+        else{
+            //its a dm
+            const { error: insertError } = await supabase.from('dm_messages').insert({
+                id,
+                content,
+                media_url,
+                is_edited: false,
+                thread_id : channel_id,
+                sender_id,
+                reply_to: reply_to || null,
+            });
 
-        if (insertError) {
-            console.error(insertError);
-            return res.status(500).json({error:'Server error'});
+            if (insertError) {
+                console.error(insertError);
+                return res.status(500).json({error:'Server error'});
+            }
         }
         return res.status(200).json({msg:'Message saved successfully'});
-    } 
+    }
     catch(error:any){
         console.error(error);
         return res.status(500).json({error:'Server error'});
