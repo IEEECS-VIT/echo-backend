b555f6849541267f108a9d660b5784d4d9fbfdd8
Aryan Ramesh Jain <138214350+jainaryan04@users.noreply.github.com>
2025-07-20 23:07:37 +0530

    server creation (#19)
    * update: dockerfile
    
    * update: server creation route, improve error handling

--- src/routes/servers.ts
+++ src/routes/servers.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import multer from 'multer';
-import {screation,getServers } from '../controllers/serverController';
+const upload = multer({ storage: multer.memoryStorage(), limits: { fileSize: 10 * 1024 * 1024 } }); // 10MB limit
+import { screation, getServers } from '../controllers/serverController';
 import { authenticate } from '../middleware/authMiddleware';
 const router = express.Router();
-const upload = multer();
 
-router.post('/create/',authenticate,upload.single('icon'),screation);
-router.get('/getServers/',authenticate,getServers);
+router.post('/create/', authenticate, upload.single('icon'), screation);
+router.get('/getServers/', authenticate, getServers);
 
 export default router;
