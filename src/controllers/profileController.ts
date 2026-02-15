82a44ed7f90beaf3cf30e2ab92cbd0f7b358393b
Labheshtheonly1 <labheshk356@gmail.com>
2026-02-15 12:13:04 +0530

    multi file processing implemented and fixed dm disappearing

--- src/controllers/profileController.ts
+++ src/controllers/profileController.ts
@@ -21,7 +21,7 @@ export const updateProfile = async (req: AuthenticatedRequest, res: Response): P
         res.status(400).json({ message: 'Invalid file type. Only images are allowed.' });
         return;
       }
-      const MAX_SIZE_IN_BYTES = 5 * 1024 * 1024; // 5MB
+      const MAX_SIZE_IN_BYTES = 50 * 1024 * 1024; // 5MB
       if (avatarFile.size > MAX_SIZE_IN_BYTES) {
         res.status(400).json({ message: 'File is too large. Maximum size is 5MB.' });
         return;
@@ -52,7 +52,7 @@ export const updateProfile = async (req: AuthenticatedRequest, res: Response): P
       }
       updateData.username = username;
     }
-  
+    
     if (bio !== undefined) updateData.bio = bio;
     if (fullname !== undefined) updateData.fullname = fullname;
 
